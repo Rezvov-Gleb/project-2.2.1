561b5586cfbe048e334302240e6d290e784b3970
Gleb 1.1.1 <gleb_rezvov@mail.ru>
2026-07-11 23:25:10 +0700

    исправления

--- src/main/java/hiber/model/Car.java
+++ src/main/java/hiber/model/Car.java
@@ -1,18 +1,10 @@
 package hiber.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.GenerationType;
-import javax.persistence.Column;
-import javax.persistence.OneToOne;
-import javax.persistence.CascadeType;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "cars")
 public class Car {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,13 +18,15 @@ public class Car {
     @OneToOne(mappedBy = "car", cascade = CascadeType.ALL)
     private User user;
 
-    public Car() {}
+    public Car() {
+    }
 
     public Car(String model, int series) {
         this.model = model;
         this.series = series;
     }
 
+    // Геттеры и сеттеры
     public Long getId() {
         return id;
     }
@@ -65,4 +59,26 @@ public class Car {
         this.user = user;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return series == car.series &&
+                java.util.Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(model, series);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
 }
